abe00fcd084c43be33a20d4856c156c85be3aeb5
Vyacheslav Shmal <vshmal@outlook.com>
2017-12-18 01:03:33 +0200

    This is the testing
    This is the change on laptop

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React New</h1>
         </header>
         <div className="container">
           <div id="container" className="col-md-8 col-md-offset-2">TESTING</div>
